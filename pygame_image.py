46ce7b818d95938c0e70ad186954508244de5916
c0b22127 <c0b2212773@edu.teu.ac.jp>
2023-11-21 15:46:00 +0900

    れんしゅう4

--- pygame_image.py
+++ pygame_image.py
@@ -9,14 +9,14 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #画像の読み込み、練習1:背景画像Surface生成
     kk_img = pg.image.load("ex01/fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)] #練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #背景の戻り値、練習4:背景画像の表示
-        screen.blit(kk_img,[300, 200])
+        screen.blit(kk_imgs[tmr%2],[300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
