e156b755d990f66b764031b0acb44a3c174c8906
c0b22127 <c0b2212773@edu.teu.ac.jp>
2023-11-21 15:58:39 +0900

    れんしゅう6

--- pygame_image.py
+++ pygame_image.py
@@ -14,12 +14,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0]) #背景の戻り値、練習4:背景画像の表示
+        
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0]) #背景の戻り値、練習4:背景画像の表示
+        screen.blit(bg_img, [1600-x, 0]) #練習6
         screen.blit(kk_imgs[tmr%2],[300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
